1e8b86035ce566fa74faba9fa5ec968e98153f5c
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2024-03-13 22:03:40 +0000

    FINAL SOLUTION

--- assignment3_starter_pack/index.js
+++ assignment3_starter_pack/index.js
@@ -35,6 +35,8 @@ const genres = [
   { id: 4, name: "Fantasy" },
 ];
 
+let nextGenreId = 5;
+
 const books = [
   { id: 1, title: "Pride and Prejudice",
     author: "Jane Austin", 
@@ -54,49 +56,157 @@ const books = [
   },
 ];
 
-/* YOUR CODE STARTS HERE */
+let nextBookId = 4;
 
-// TODO: Implement all logic from the assignment desription
+/* YOUR CODE STARTS HERE */
 
 //GET request for all books
 app.get(apiPath + version + "/books", (req, res) => {
-  const filter = req.query.filter;
-  let filteredBooks = [];
+  //Check if there is a query
+  const queryKeys = Object.keys(req.query)
+  
+  //If no query return full json list
+  if(queryKeys.length === 0){
+    res.status(200).json(books);
+  }
 
-  if (filter) {
-    // Filter books based on the genre
-    const filterGenreId = genres.find(genre => genre.name === filter);
+  //Check if the correct query is being used
+  if(queryKeys[0] === 'filter' && queryKeys.length === 1){
+    const filter = req.query.filter;
 
-    books.forEach(book => {
-      book.genreId === filterGenreId;
-      filteredBooks.push(book)
-    })
+    const filterGenre = genres.find(genre => genre.name.toLowerCase() === filter.toLowerCase());
 
-    // Respond with the filtered list of books
+    //If no books are found, return an empty array
+    if(!filterGenre){
+          res.status(200).json([]);
+      }
+
+    let filteredBooks = books.filter(book => book.genreId === filterGenre.id);
     res.status(200).json(filteredBooks);
   } else {
-    res.status(200).json(books);
+    res.status(400).json({
+      message: "Invalid filter applied"
+    })
   }
 });
 
-//GET request to return a specific book by its id
-app.get(apiPath + version + "/books/:id", (req, res) => {
+//GET request to return a specific book by its ID in a specific genre
+app.get(apiPath + version + "/genres/:genreId/books/:id", (req, res) => {
+  const inpBookId = +req.params.id;
+  const inpGenreId = +req.params.genreId;
 
+  const book = books.find(book => book.id === inpBookId && book.genreId === inpGenreId);
+  //If no book is found, return 404 status
+  if (!book){
+      res.status(404).json({
+      message: "There was no book found with the given ID in the specified genre."
+    });
+  }
+  res.status(200).json(book)
 });
 
 //POST request to create a new book within a specific genre
 app.post(apiPath + version + "/genres/:genreId/books", (req, res) => {
+  const {title,author} = req.body
 
+  //Check if necessary information is available
+  if (!title || !author)
+    res.status(400).send({
+    message: "Required fields missing."
+  })
+  const genreId = +req.params.genreId;
+
+  const genre = genres.find(genre => genre.id === genreId);
+
+  //Check if the provided genre exists
+  if (!genre)
+      res.status(400).send({
+      message: "The provided genre was not found."
+    })
+  const newId = nextBookId;
+  const book = {
+    id: newId,
+    title,
+    author,
+    genreId
+  };
+
+  books.push(book)
+  nextBookId++;
+
+  res.status(201).json(book);
 });
 
 //PATCH request to partially update a book by id
-app.patch(apiPath + version + "/books/:id", (req, res) => {
+app.patch(apiPath + version + "/genres/:genreId/books/:id", (req, res) => {
+  const {title,author,genreId} = req.body;
+  const inpBookId = +req.params.id;
+  const inpGenreId = +req.params.genreId;
 
+  const book = books.find(book => book.id === inpBookId);
+
+  //Check if there is a book with the provided id, else return status 400 (Not 404 for some reason?)
+  if (!book) {
+      res.status(400).json({
+      message: "There was no book found with the given ID."
+    })
+  }
+  const genre = genres.find(genre => genre.id === inpGenreId);
+
+  //Check if there is a genre with the provided id, else return status 404
+  if (!genre)
+      res.status(404).json({
+      message: "This genre was not found."
+    })
+
+  //Check if the given books genre and the given genre match
+  if (book.genreId !== inpGenreId){
+      res.status(404).json({
+      message: "There is a mismatch between the books genre and the supposed genre ID provided."
+    })
+  }
+
+  if (title) {
+    book.title = title;
+  }
+  if (author) {
+    book.author = author;
+  }
+  if (genreId) {
+    book.genreId = genreId;
+  }
+  res.status(200).json(book);
 });
+
+//DELETE request for deleting all books, not allowed
+app.delete(apiPath + version + "/books", (req, res) => {
+  res.status(405).json({
+    message: "Method Not Allowed",
+  })
+})
 
 //DELETE request to delete a book by id
 app.delete(apiPath + version + "/books/:id", (req, res) => {
+  const inpId = +req.params.id;
 
+  //Catch any non numerical or negative id's
+  if(inpId < 0 || isNaN(inpId)){
+    res.status(400).json({
+      message: "Id must be a valid id",
+    })
+  }
+
+  //Find the index of the given book id
+  const bookIndex = books.findIndex(book => book.id === inpId);
+
+  //If no book with the given id is found return 404 status
+  if(bookIndex == -1){
+    res.status(404).json({
+      message: "There was no book found with the given ID.",
+    });
+  }
+  const delBook = books.splice(bookIndex, 1);
+  res.status(200).json(delBook);
 });
 
 //GET request for all genres
@@ -106,16 +216,18 @@ app.get(apiPath + version + "/genres", (req, res) => {
 
 //POST request for genres
 app.post(apiPath + version + "/genres", (req, res) => {
-  
+  //Check if the provided name is of the correct format
   if(!req.body.name || typeof req.body.name !== 'string' || req.body.name.trim().length === 0) {
     return res.status(400).json({
-      message: "Genres require at valid name.",
+      message: "Genres require a valid name.",
     });
   }
   
   let nameExists = false;
 
   genres.forEach(element => {
+
+    //Check if the genre does not already exist
     if(req.body.name.toLowerCase() === element.name.toLowerCase()){
       nameExists = true;
     };
@@ -132,8 +244,49 @@ app.post(apiPath + version + "/genres", (req, res) => {
   };
   genres.push(newGenre);
   nextGenreId++;
-  res.status(201).json(newGenre);
 
+  res.status(201).json(newGenre);
+})
+
+//DELETE request for deleting all genres, not allowed
+app.delete(apiPath + version + "/genres", (req, res) => {
+  res.status(405).json({
+    message: "Method Not Allowed",
+  })
+})
+
+//DELETE request for deleting a genre by id
+app.delete(apiPath + version + "/genres/:id", (req, res) => {
+  const inpId = +req.params.id;
+
+  //Catch any non numerical or negative id's
+  if(inpId < 0 || isNaN(inpId)){
+    res.status(400).json({
+      message: "Id must be a valid id",
+    })
+  }
+
+  //Find the index of the given genre id
+  const genreIndex = genres.findIndex(genre => genre.id === inpId);
+
+  //If no genre is found return 404 status
+  if(genreIndex == -1){
+    res.status(404).json({
+      message: "There was no genre found with the given ID.",
+    });
+  }
+
+  book = books.find(book => book.genreId == inpId);
+
+  //If the genre still has books, forbid its deletion
+  if(book !== undefined){
+    res.status(400).json({
+      message: "Method Not Allowed",
+    });
+  }
+
+  const delGenre = genres.splice(genreIndex, 1);
+  res.status(200).json(delGenre);
 })
 
 /* YOUR CODE ENDS HERE */
